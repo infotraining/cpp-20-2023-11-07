82124745e81754b018f25f295d0f08d887d5622e
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-07 09:27:18 +0000

    Compare integrals

--- compare/comparisons.cpp
+++ compare/comparisons.cpp
@@ -2,10 +2,34 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <utility>
 
 using namespace std::literals;
 
-TEST_CASE("operator ==")
+TEST_CASE("safe comparisons between integral numbers")
 {
-    REQUIRE(true);
+    int x = -7;
+    unsigned y = 42;
+
+    //CHECK(x < y);
+    CHECK(std::cmp_less(x, y));
+    CHECK(std::cmp_greater_equal(y, x));
+
+    CHECK(std::in_range<size_t>(-1) == false);
+    CHECK(std::in_range<size_t>(665) == true);
+
+    //auto my_cmp_less = [](std::integral auto a, std::integral auto b) { return std::cmp_less(a, b); };
+    auto my_cmp_less = [](auto a, auto b) { 
+        if constexpr(std::is_integral_v<decltype(a)> && std::is_integral_v<decltype(b)>)
+            return std::cmp_less(a, b);
+        else
+            return a < b; 
+    };
+
+    CHECK(my_cmp_less(x, y));
+
+    std::vector vec = {1, 2, 3};
+    CHECK(my_cmp_less(x, vec.size()));
+
+    CHECK(my_cmp_less("test"s, "text"s));
 }
--- ranges/ranges_and_views.cpp
+++ ranges/ranges_and_views.cpp
@@ -9,7 +9,7 @@ using namespace std::literals;
 
 TEST_CASE("ranges")
 {
-    constexpr auto data = helpers::create_numeric_dataset<100>(42);
+    auto data = helpers::create_numeric_dataset<100>(42);
     helpers::print(data, "data");
 
     auto runtime_data = helpers::create_numeric_dataset<100>(42);
